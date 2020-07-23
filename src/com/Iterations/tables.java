499d307863b4735b204bbbe971aadcb9fa352743
GN Vageesh <vageeshgn2005@gmail.com>
2020-07-23 13:00:19 +0530

    Added Tables Program.

---
+++ src/com/Iterations/tables.java
@@ -0,0 +1,18 @@
+package com.Iterations;
+
+import java.util.Scanner;
+
+public class tables
+{
+    public static void main(String[] args)
+    {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter a number");
+        int n = sc.nextInt();
+
+        for(int i = 1; i <=10; i++)
+        {
+            System.out.println(n+" x "+i+" = "+(n*i));
+        }
+    }
+}
